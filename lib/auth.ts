e89a9ea7f6d3de37917fe613b0d68798034075a4
Rashedul Islam Maruf <mdm326040@gmail.com>
2025-12-24 20:00:13 +0600

    fix auth.ts type issue

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,17 +3,8 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { nextCookies } from "better-auth/next-js";
 import prisma from "./prisma";
 
-const prismaProxy = new Proxy(prisma, {
-  get(target, prop) {
-    if (typeof prop === "string" && prop[0] === prop[0].toUpperCase()) {
-      return target[prop.toLowerCase()];
-    }
-    return target[prop];
-  },
-});
-
 export const auth = betterAuth({
-  database: prismaAdapter(prismaProxy, {
+  database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
   emailAndPassword: {
